295744adda48b53e21ac4d95319386b0c385a2f6
Clark <calrk1@gmail.com>
2014-07-12 22:10:53 +1000

    Adding upload to s3 to the build script, for automatically updating the examples website

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var connect = require('gulp-connect');
+var s3 = require('gulp-s3');
+var fs = require('fs');
 
 var paths = {
 	js : ['./src/*.js', './src/**/*.js']
@@ -18,6 +20,7 @@ gulp.task('js', function(){
 	gulp.src(paths.js)
 		.pipe(concat('clarity.min.js'))
 		.pipe(uglify())
+		.pipe(gulp.dest('./examples/js/'))
 		.pipe(gulp.dest('./build/'));
 });
 
@@ -30,4 +33,11 @@ gulp.task('connect', function () {
 
 gulp.task('watch', function () {
 	gulp.watch(paths.js, ['js']);
+});
+
+gulp.task('aws', function(){
+	aws = JSON.parse(fs.readFileSync('./aws.json'));
+
+	gulp.src('./examples/**')
+		.pipe(s3(aws));
 });
